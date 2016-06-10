f7e9ccd298de470f92541c5d1d60930f7d7c605c
sdossettswift <sdossettswift@gmail.com>
2016-06-09 23:27:18 -0500

    yay bob

--- bob.rb
+++ bob.rb
@@ -1,2 +1,24 @@
 class Bob
+    def hey(remark)
+        ##SILENCE
+        if (remark == remark.reverse)
+            'Fine. Be that way!'
+            ##ALL CAPS (But not all numbers)
+        elsif (remark == remark.upcase) && (remark.downcase != remark.upcase)
+            'Whoa, chill out!'
+            ##QUESTION
+        elsif remark.end_with?("?")
+            'Sure.'
+            ##CATCHALL
+        else
+            'Whatever.'
+        end
+    end
 end
+
+bob = Bob.new
+puts bob.hey "Howdy!"
+puts bob.hey "What's up?"
+puts bob.hey "LEAVE ME ALONE!"
+puts bob.hey "8675309"
+puts bob.hey "          "
--- bob_test.rb
+++ bob_test.rb
@@ -15,116 +15,97 @@ class BobTest < Minitest::Test
         assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
     end
 
-    def test_shouting
-        skip
+    def test_shouting #all caps, exclaimation
         remark = 'WATCH OUT!'
         assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
     end
 
-    def test_shouting_gibberish
-        skip
+    def test_shouting_gibberish # alll caps (no exclaimation)
         remark = ('A'..'Z').to_a.shuffle[0, 10].join
         assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
     end
 
-    def test_asking_a_question
-        skip
+    def test_asking_a_question #ends w/ ?
         remark = 'Does this cryogenic chamber make me look fat?'
         assert_equal 'Sure.', bob.hey(remark), feedback(remark)
     end
 
-    def test_asking_a_numeric_question
-        skip
+    def test_asking_a_numeric_question #ends w/ ?
         remark = 'You are, what, like 15?'
         assert_equal 'Sure.', bob.hey(remark), feedback(remark)
     end
 
-    def test_asking_gibberish
-        skip
+    def test_asking_gibberish #ends w/ ?
         remark = ('a'..'z').to_a.shuffle[0, 10].join << '?'
         assert_equal 'Sure.', bob.hey(remark), feedback(remark)
     end
 
-    def test_talking_forcefully
-        skip
+    def test_talking_forcefully #! but not yelling (ending w/ ! is nonevent)
         remark = "Let's go make out behind the gym!"
         assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
     end
 
-    def test_using_acronyms_in_regular_speech
-        skip
+    def test_using_acronyms_in_regular_speech #not all caps, but some caps
         remark = "It's OK if you don't want to go to the DMV."
         assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
     end
 
-    def test_forceful_questions
-        skip
+    def test_forceful_questions #all caps over ending w/ ? yelling needs to come before questions
         remark = 'WHAT THE HELL WERE YOU THINKING?'
         assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
     end
 
-    def test_shouting_numbers
-        skip
+    def test_shouting_numbers #all caps
         remark = '1, 2, 3 GO!'
         assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
     end
 
-    def test_only_numbers
-        skip
+    def test_only_numbers #counts as all caps, so need to make all caps exclude all numbers
         remark = '1, 2, 3'
         assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
     end
 
-    def test_question_with_only_numbers
-        skip
+    def test_question_with_only_numbers #question needs to be ends w/ ?
         remark = '4?'
         assert_equal 'Sure.', bob.hey(remark), feedback(remark)
     end
 
-    def test_shouting_with_special_characters
-        skip
+    def test_shouting_with_special_characters #all caps, special characters irrelevant
         remark = 'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!'
         assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
     end
 
-    def test_shouting_with_no_exclamation_mark
-        skip
+    def test_shouting_with_no_exclamation_mark #again - yelling is defined by all caps
         remark = 'I HATE YOU'
         assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
     end
 
-    def test_statement_containing_question_mark
-        skip
+    def test_statement_containing_question_mark # question must END w/ ?, not just contain it
         remark = 'Ending with ? means a question.'
         assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
     end
 
-    def test_prattling_on
-        skip
+    def test_prattling_on # end w/ question. ! irrelevant
         remark = 'Wait! Hang on. Are you going to be OK?'
         assert_equal 'Sure.', bob.hey(remark), feedback(remark)
     end
 
-    def test_silence
-        skip
+    def test_silence #silence of any length
         remark = ''
         assert_equal 'Fine. Be that way!', bob.hey(remark), feedback(remark)
     end
 
     def test_prolonged_silence
-        skip
         remark = ' ' * rand(1..10)
         assert_equal 'Fine. Be that way!', bob.hey(remark), feedback(remark)
     end
 
     def test_alternate_silences
-        skip
         remark = "\t" * rand(1..10)
         assert_equal 'Fine. Be that way!', bob.hey(remark), feedback(remark)
     end
 
     def test_on_multiple_line_questions
-        skip
         remark = %(
 Does this cryogenic chamber make me look fat?
 no)
